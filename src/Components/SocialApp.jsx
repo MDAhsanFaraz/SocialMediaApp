967c378862d919bd00937ca9535f22882b0b6a3d
Faraz <faraz.com>
2024-02-21 07:59:10 +0530

    Added Maincontent

--- src/Components/SocialApp.jsx
+++ src/Components/SocialApp.jsx
@@ -1,10 +1,11 @@
 import PrimarySearchAppBar from "./Navbar/Navbar";
 import PostCard from "./PostCard";
-
+import MainContainer from "././MainContainer.jsx/MainContainer";
 function SocialApp() {
   return (
     <div>
       <PrimarySearchAppBar />
+      <MainContainer />
       Social App
       <PostCard />
       <PostCard />
