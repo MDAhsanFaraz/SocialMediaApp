2043489e942e373c564ed49becfa54ef37e0d295
Faraz <faraz.com>
2024-02-29 01:39:29 +0530

    more custom hooks to  segregated logic

--- src/Components/SocialApp.jsx
+++ src/Components/SocialApp.jsx
@@ -1,23 +1,9 @@
-import Navbar from "./Navbar/Navbar";
 import Input from "./Input/Input";
 import MainContainer from "././MainContainer.jsx/MainContainer";
 import PostsContext from "../providers/PostsProvider";
-import { useState, useEffect } from "react";
-import axios from "axios";
-
+import usePosts from "../hooks/usePosts";
 function SocialApp() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    // we will download the content from dummyapi.io
-    axios
-      .get("https://dummyapi.io/data/v1/post", {
-        headers: { "app-id": import.meta.env.VITE_APP_ID },
-      })
-      .then((response) => {
-        const responseObject = response.data;
-        setPosts([...responseObject.data]);
-      });
-  }, []);
+  const [posts, setPosts] = usePosts();
   return (
     <div>
       <PostsContext.Provider value={{ posts, setPosts }}>
